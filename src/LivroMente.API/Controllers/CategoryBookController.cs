49886c1daae212fd2681916aebbd213d60e5003c
yasmin.borges <yasmin.borges@tandersolucoes.com>
2023-10-15 12:15:19 -0300

    Crud_Category_Completed

--- src/LivroMente.API/Controllers/CategoryBookController.cs
+++ src/LivroMente.API/Controllers/CategoryBookController.cs
@@ -5,64 +5,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LivroMente.API.Controllers
 {
-     [Route("api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CategoryBookController : ControllerBase
     {
           private readonly ICategoryBookRepository _categoryBookRepository;
+          private readonly IMapper _mapper;
 
-        public CategoryBookController(ICategoryBookRepository categoryBookRepository)
+        public CategoryBookController(ICategoryBookRepository categoryBookRepository,IMapper mapper)
         {
+            _mapper = mapper;
             _categoryBookRepository = categoryBookRepository;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add([FromBody] CategoryBookViewModel request)
+
+        public async Task<IActionResult> Post(CategoryBookViewModel category)
         {
-            // Map ViewModel to Domain Model
-            var category = new CategoryBook
-            {
-                Id= Guid.NewGuid(),
-                Description= request.Description,
-                IsFree= request.IsFree,
-            };
+            var cat1 = _mapper.Map<CategoryBook>(category);
+             _categoryBookRepository.Add(cat1);
 
-            _categoryBookRepository.Add(category);
-            // Domain model to ViewModel
-            var response = new CategoryBookViewModel
-            {
-                Id = category.Id,
-                Description = category.Description,
-                IsFree= category.IsFree
-            };
-            
-            if(await _categoryBookRepository.UnitOfWork.SaveChangesAsync() > 0){
-                 return Created($"api/CategoryBook/{category.Id}", category);
-            }
-            
-
+             if (await _categoryBookRepository.UnitOfWork.SaveChangesAsync() > 0)
+                return Created($"api/CategoryBook/{category.Id}", category);
             return BadRequest();
         }
 
-        // GET: https://localhost:7107/api/Category
         [HttpGet]
         public  async Task<IActionResult> GetAllCategories()
         {
             var categories = await  _categoryBookRepository.GetAll();
-
-            // Map Domain model to DTO
-
-            //var response = new List<CategoryDto>();
-            //foreach (var category in caterogies)
-            //{
-            //    response.Add(new CategoryDto
-            //    {
-            //        Id = category.Id,
-            //        Name = category.Name,
-            //        UrlHandle = category.UrlHandle
-            //    });
-            //}
-
             return Ok(categories);
         }
 
@@ -75,42 +46,32 @@ namespace LivroMente.API.Controllers
         }
 
         [HttpPut("{CategoryId}")]
-        public async Task<IActionResult> Put(Guid CategoryId,[FromBody] CategoryBookViewModel request)
+        public async Task<IActionResult> Put(Guid CategoryId,CategoryBookViewModel category)
         {
-             var entity =   _categoryBookRepository.GetbyId(CategoryId);
+             var entity =  _categoryBookRepository.GetbyId(CategoryId);
 
-             if (entity == null) return NotFound();
-
-             var category = new  CategoryBookViewModel
-             {
-                Id = entity.Id,
-                Description= request.Description,
-                IsFree= request.IsFree,
-             }; 
-              
-              _categoryBookRepository.Update(entity);
+            if (entity == null) return NotFound();
+            _mapper.Map(category, entity);
+            _categoryBookRepository.Update(entity);
 
             if(await _categoryBookRepository.UnitOfWork.SaveChangesAsync() > 0)
-            return Created($"api/CategoryBook/{category.Id}", category);
-
+          
+            return Created($"api/CategoryBook/{category.Id}", _mapper.Map<CategoryBookViewModel>(entity));
             return BadRequest();
         }
 
         [HttpDelete ("{CategoryId}")]
         public async Task<IActionResult> Delete(Guid CategoryId)
         {
-            var entity = _categoryBookRepository.GetbyId(CategoryId);
-           
-             if (entity == null) return NotFound();
+              var entity =  _categoryBookRepository.GetbyId(CategoryId);
+
+            if (entity == null) return NotFound();
             _categoryBookRepository.Delete(entity);
 
-          var result =  await _categoryBookRepository.UnitOfWork.SaveChangesAsync() > 0;
-
-          if(result)
+            if (await _categoryBookRepository.UnitOfWork.SaveChangesAsync() > 0)
             return Ok();
-            
-            return BadRequest();  
-        }
+            return BadRequest(); 
 
     }
+}
 }
